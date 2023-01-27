de152f92070c0590d5421dcd8e2ce70227bfd4d1
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-27 19:21:05 -0300

    Update array.js

--- 002_Desafio/002/array.js
+++ 002_Desafio/002/array.js
@@ -1,4 +1,16 @@
+// inserindo itens 
+/*const num = [7, 8, 9]
 
-let friends = ['Marcos', 'Silvia', 'Elisa' ]
-friends[1] = 'Junior'
-console.log(friends)
+num.push(10, 11, 12) 
+num.unshift(1, 2, 3)
+num.splice(3, 0, 4, 5, 6 )
+
+console.log(num)*/
+
+const movies = [
+   {id: 1, movieName: 'Dejavu'},
+   {id: 2, movieName: 'Back to the future'},
+   {id: 3, movieName: 'The Matrix'}
+]
+
+console.log(movies)
