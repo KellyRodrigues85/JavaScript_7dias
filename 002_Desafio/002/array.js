6f76f9c0a6ebb65135805346b9654b4f35c0f3b2
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-28 11:43:07 -0300

    Update array.js

--- 002_Desafio/002/array.js
+++ 002_Desafio/002/array.js
@@ -7,10 +7,32 @@ num.splice(3, 0, 4, 5, 6 )
 
 console.log(num)*/
 
-const movies = [
+/*const movies = [
    {id: 1, movieName: 'Dejavu'},
    {id: 2, movieName: 'Back to the future'},
    {id: 3, movieName: 'The Matrix'}
 ]
 
-console.log(movies)
+console.log(movies)*/
+
+/*const num =  [5, 6, 7, 8]
+// const final= num.pop() // remove o ultimo numero//
+//const final= num.shift() //remove o primeiro numero
+const final= num.splice(2, 1) // remove partes, apartir da posição do index (2) e quantos itens a partir da possição dois será retirado (1)
+
+console.log(num)
+console.log(final)*/
+/*
+//FRORMAS DE ZERAR O ARRAY
+let num = [5, 6, 7, 8]
+//num = []
+//h = 0
+num.splice(0, num.lengt)
+
+console.log(num)*/
+
+// JUNTANDO ARRAYS
+
+
+
+
