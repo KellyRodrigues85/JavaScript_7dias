76fcd3e87f5c63d66e5d7ef6c4a4a6990e1725dd
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-30 19:35:15 -0300

    doing the function

--- projetos/Tip/Antes/index.js
+++ projetos/Tip/Antes/index.js
@@ -1,3 +1,39 @@
+
 function update(){
-    console.log('ok')
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent/100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+    
+    document.getElementById('tipPercent').innerHTML= tipPercent + ' % '
+
+    document.getElementById('tipValue').innerHTML=  '$ ' + tipValue
+
+    document.getElementById('totalWithTip').innerHTML= '$ ' + billTotal
+
+    document.getElementById('splitValue').innerHTML = split
+
+    document.getElementById('billEach').innerHTML = '$ ' + billEach
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
