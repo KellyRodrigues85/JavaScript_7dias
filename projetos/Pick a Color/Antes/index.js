51efef702c21e36fe88e826e898b84fe44f2b23f
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-02-01 18:39:52 -0300

    Update index.js

--- projetos/Pick a Color/Antes/index.js
+++ projetos/Pick a Color/Antes/index.js
@@ -1,2 +1,16 @@
-const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green', 'Black']
 const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+// no click
+btn.addEventListener('click', function(){
+    const randowColor = getRandowColor()
+    document.body.style.background = colors[randowColor]
+    color.textContent = colors[randowColor]
+    
+})
+
+// gerar os numeros randomicos
+function getRandowColor(){
+    return Math.floor(Math.random() * colors.length)
+}
