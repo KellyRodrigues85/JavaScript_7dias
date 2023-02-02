2d74524ba0f296e85ef9ad4ce747a2614acd8079
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-02-02 12:42:30 -0300

    initial html

---
+++ projetos/Guess the Number/Antes/index.js
@@ -0,0 +1,22 @@
+let computerNumber
+let userNumbers = []
+
+function init() {
+    computerNumber =  Math.floor(Math.random() * 100 + 1)
+    console.log(computerNumber)  
+}
+
+function compareNumbers(){
+    const userNumber =  Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if(userNumber > computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+
+    } else if (userNumber < computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Your number is too low'
+    } else {
+       document.getElementById('textOutput').innerHTML = 'Congratulations you win!!!'       
+    }
+}
