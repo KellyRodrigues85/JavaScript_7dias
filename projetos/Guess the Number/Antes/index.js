b70854da7616c53c7edc20e0775875c9ac154a18
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-02-02 12:49:21 -0300

    improving

--- projetos/Guess the Number/Antes/index.js
+++ projetos/Guess the Number/Antes/index.js
@@ -3,7 +3,7 @@ let userNumbers = []
 
 function init() {
     computerNumber =  Math.floor(Math.random() * 100 + 1)
-    console.log(computerNumber)  
+    //console.log(computerNumber)  
 }
 
 function compareNumbers(){
@@ -14,8 +14,12 @@ function compareNumbers(){
     if(userNumber > computerNumber) {
         document.getElementById('textOutput').innerHTML = 'Your number is too high'
 
+        document.getElementById('inputBox').value = ''
+
     } else if (userNumber < computerNumber){
         document.getElementById('textOutput').innerHTML = 'Your number is too low'
+
+        document.getElementById('inputBox').value = ''
     } else {
        document.getElementById('textOutput').innerHTML = 'Congratulations you win!!!'       
     }
